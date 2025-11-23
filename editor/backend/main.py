d20ccad0f7e26f1bd198089f8c6eb83283693e32
joaobgoode <joaovictorvvm@edu.unifor.br>
2025-11-23 17:27:14 -0300

    fix cors for development

--- editor/backend/main.py
+++ editor/backend/main.py
@@ -1,29 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  # <--- 1. Importe isso
+from fastapi.staticfiles import StaticFiles
 from editor.backend.app.routers.deck import router as deck_router
 from editor.backend.app.routers.card import router as card_router
 from editor.backend.app.routers.commander import router as commander_router
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
 from infra.config import settings
-
 
 app = FastAPI(
     title="MTG Commanders App",
     version="0.1.0",
 )
 
-origins = ["*"]
+origins = [
+    "http://localhost:5173", 
+    "http://localhost:3000", 
+    "http://127.0.0.1:5173",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=origins,    
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],       
+    allow_headers=["*"],       
 )
 
-
-app = FastAPI()
 app.include_router(card_router)
 app.include_router(deck_router)
 app.include_router(commander_router)
